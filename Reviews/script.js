3fdcbdf8476598ddf89fa56f0b4f1c764c4410a0
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-08-20 22:08:24 +0530

    reviews

--- Reviews/script.js
+++ Reviews/script.js
@@ -1,6 +1,6 @@
 const items=[
     {
-        img: "../img/person-1.jpg",
+        img: "img/person-1.jpg",
         name: "Susan Smith",
         designation: "WEB DEVELOPER",
         review:" I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
