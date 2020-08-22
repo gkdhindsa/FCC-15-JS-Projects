f782e16f4dfcb60197bf01f0cafdc1915efcfbcc
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-08-22 21:24:07 +0530

    navbar

--- Navbar/script.js
+++ Navbar/script.js
@@ -1,2 +1,10 @@
-
-console.log(window.innerHeight)
+const navToggle=document.querySelector('.nav-toggle');
+const links=document.querySelector('.links');
+navToggle.addEventListener('click', function(){
+    if(links.classList.contains('show-links')){
+        links.classList.remove('show-links')
+    }
+    else{
+        links.classList.add('show-links');
+    }
+})
